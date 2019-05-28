87380332daee9d2594bd816e840cbd4dbb938479
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-05-28 09:50:46 +0200

    Little cleanup.

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversity.Models
 {
-    public class SchoolContext : DbContext
-    {
-        public SchoolContext (DbContextOptions<SchoolContext> options)
-            : base(options)
-        {
-        }
+	public class SchoolContext : DbContext
+	{
+		public SchoolContext(DbContextOptions<SchoolContext> options)
+				: base(options)
+		{
+		}
 
-        public DbSet<ContosoUniversity.Models.Student> Student { get; set; }
-    }
+		public DbSet<ContosoUniversity.Models.Student> Student { get; set; }
+	}
 }
--- Pages/Students/Create.cshtml.cs
+++ Pages/Students/Create.cshtml.cs
@@ -1,42 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using ContosoUniversity.Models;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Pages.Students
 {
-    public class CreateModel : PageModel
-    {
-        private readonly ContosoUniversity.Models.SchoolContext _context;
+	public class CreateModel : PageModel
+	{
+		private readonly ContosoUniversity.Models.SchoolContext _context;
 
-        public CreateModel(ContosoUniversity.Models.SchoolContext context)
-        {
-            _context = context;
-        }
+		public CreateModel(ContosoUniversity.Models.SchoolContext context)
+		{
+			_context = context;
+		}
 
-        public IActionResult OnGet()
-        {
-            return Page();
-        }
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
 
-        [BindProperty]
-        public Student Student { get; set; }
+		[BindProperty]
+		public Student Student { get; set; }
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
 
-            _context.Student.Add(Student);
-            await _context.SaveChangesAsync();
+			_context.Student.Add(Student);
+			await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
-        }
-    }
+			return RedirectToPage("./Index");
+		}
+	}
 }
