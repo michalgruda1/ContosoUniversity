6aa72fb71d6ef02aab0d7d185055aeb14ba2b18b
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-05-28 12:31:31 +0200

    Added info for schema to have 3 sets of entities.

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -9,6 +9,8 @@ namespace ContosoUniversity.Models
 		{
 		}
 
-		public DbSet<ContosoUniversity.Models.Student> Student { get; set; }
+		public DbSet<Student> Student { get; set; }
+		public DbSet<Enrollment> Enrollment { get; set; }
+		public DbSet<Course> Course { get; set; }
 	}
 }
