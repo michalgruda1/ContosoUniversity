67e2d44ce21f77ac5ccc5e3be9a75ce9477db5e9
Michal Gruda <michal.gruda@sygenic.com>
2019-07-02 14:58:27 +0200

    Moved to .Net Core 2.2 to enable proper Razor Pages tag coloring. Updated dependancies. Added pagination to Students.

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Pages.Students
 {
