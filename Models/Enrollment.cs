62d51d8bc1f701f67f5b496361290f6565afb2a9
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-07-03 11:44:37 +0200

    Added Department entity.

--- Models/Enrollment.cs
+++ Models/Enrollment.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -10,11 +8,14 @@ namespace ContosoUniversity.Models
 	public class Enrollment
 	{
 		public int EnrollmentID { get; set; }
-		public int CourseID { get; set; }
-		public int StudentID { get; set; }
+
+		[DisplayFormat(NullDisplayText = "No grade")]
 		public Grade? Grade { get; set; }
 
+		public int CourseID { get; set; }
 		public Course Course { get; set; }
+
+		public int StudentID { get; set; }
 		public Student Student { get; set; }
 	}
 }
