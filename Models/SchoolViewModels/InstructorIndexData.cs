1442620dc5fcf8c093ff1ea1938b4120a3a9ab33
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-07-04 12:32:53 +0200

    ViewModel class addes, to display custom instructor index page

---
+++ Models/SchoolViewModels/InstructorIndexData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ContosoUniversity.Models.SchoolViewModels
+{
+	public class InstructorIndexData
+	{
+		public virtual IEnumerable<Instructor> Instructors { get; set; }
+		public virtual IEnumerable<Course> Courses { get; set; }
+		public virtual IEnumerable<Enrollment> Enrollments { get; set; }
+	}
+}
