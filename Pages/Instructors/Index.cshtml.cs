6003991f32629999f81c41028316a1c9d2f4b0c4
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-07-04 12:34:03 +0200

    Addes scaffolded CRUD Razor Pages for Instructor class

---
+++ Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Pages.Instructors
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ContosoUniversity.Models.SchoolContext _context;
+
+		public IndexModel(ContosoUniversity.Models.SchoolContext context)
+		{
+			_context = context;
+		}
+
+		public IList<Instructor> Instructor { get; set; }
+
+		public async Task OnGetAsync()
+		{
+			Instructor = await _context.Instructors.ToListAsync();
+		}
+	}
+}
