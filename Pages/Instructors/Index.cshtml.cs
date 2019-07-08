f8586fdc31408acba24a59c81b7c5660287be3f8
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-07-08 16:56:06 +0200

    x

--- Pages/Instructors/Index.cshtml.cs
+++ Pages/Instructors/Index.cshtml.cs
@@ -2,7 +2,6 @@
 using ContosoUniversity.Models.SchoolViewModels;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
