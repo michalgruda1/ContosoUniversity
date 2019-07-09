8048b7e40ace351c4563bb146dba0d557e5d8373
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-07-09 14:51:54 +0200

    Added Edit and Display office assignment.

--- Pages/Instructors/Edit.cshtml.cs
+++ Pages/Instructors/Edit.cshtml.cs
@@ -1,74 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Models;
+using System;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Pages.Instructors
 {
-    public class EditModel : PageModel
-    {
-        private readonly ContosoUniversity.Models.SchoolContext _context;
+	public class EditModel : PageModel
+	{
+		private readonly SchoolContext _context;
 
-        public EditModel(ContosoUniversity.Models.SchoolContext context)
-        {
-            _context = context;
-        }
+		public EditModel(SchoolContext context)
+		{
+			_context = context;
+		}
 
-        [BindProperty]
-        public Instructor Instructor { get; set; }
+		[BindProperty]
+		public Instructor Instructor { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            Instructor = await _context.Instructors.FirstOrDefaultAsync(m => m.ID == id);
+			Instructor = await _context.Instructors
+					.Include(i => i.OfficeAssignment)
+					.AsNoTracking()
+					.FirstOrDefaultAsync(m => m.ID == id);
 
-            if (Instructor == null)
-            {
-                return NotFound();
-            }
-            return Page();
-        }
+			if (Instructor == null)
+			{
+				return NotFound();
+			}
+			return Page();
+		}
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+		public async Task<IActionResult> OnPostAsync(int? id)
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
 
-            _context.Attach(Instructor).State = EntityState.Modified;
+			var instructorToUpdate = await _context.Instructors
+					.Include(i => i.OfficeAssignment)
+					.FirstOrDefaultAsync(s => s.ID == id);
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!InstructorExists(Instructor.ID))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+			if (await TryUpdateModelAsync<Instructor>(
+					instructorToUpdate,
+					nameof(Models.Instructor),
+					i => i.FirstMidName, i => i.LastName,
+					i => i.HireDate, i => i.OfficeAssignment))
+			{
+				if (String.IsNullOrWhiteSpace(
+						instructorToUpdate.OfficeAssignment?.Location))
+				{
+					instructorToUpdate.OfficeAssignment = null;
+				}
+				await _context.SaveChangesAsync();
+			}
+			return RedirectToPage("./Index");
 
-            return RedirectToPage("./Index");
-        }
-
-        private bool InstructorExists(int id)
-        {
-            return _context.Instructors.Any(e => e.ID == id);
-        }
-    }
+		}
+	}
 }
